5ee45058942ca812607824a92c908fc806b8fcbb
carmenuv <carmenurizarv@gmail.com>
2022-09-14 23:29:38 -0500

    Inicio de proyecto

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Listausuarios.css';
+
+const Listausuarios = () => {
+  return(
+    <div>
+      <h1><Inicio></Inicio></h1>
+    </div>
+  )
+
+}
+
+export {Listausuarios};
